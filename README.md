10741e909e26ea04680d6268b7bc0a0e9f626727
adepase adedolamu <adepaseadedolamu@gmail.com>
2024-02-06 19:36:47 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## GETTING STARTED WITH PYTHON
+![ans](./images/0.jpg)
+### What is Python
+Python is a versatile, high-level programming language known for its readability and ease of use. It supports multiple paradigms, making it suitable for diverse applications, from web development to data analysis and artificial intelligence.
+>#### Types of Python Input
+- Variables:In Python, variables are containers for storing data values. Variables can hold different types of data, such as numbers, strings, or objects, and their values can be changed during the program execution.
+>###### Writing variables
+![var1](./images/var1.png)
+### Data types in Python
+- Integers:Integers in Python are whole numbers, both positive and negative, without any decimal or fractional part. They can be used for arithmetic operations and are represented without quotes. For example, 5, -3, and 0 are integers in Python.
+>###### NB: Integers are always whole numbers. i.e 10,20,30 & 100.
+![int1](./images/int1.png)
+
+- Floats: In shorts, Floats in Python are numeric data types that represent numbers with decimal points.
+![flo1](./images/flo1.png) 
+- Strings: a series of text within quotes,either a single or double quote("",'').
+![stri1](./images/stri1.png)
+
+- Boolean: can only have two states. true or false. Yes or no,1 or 0.
+![boo1](./images/boo1.png)
+
+
---
+++ input.py
@@ -0,0 +1,103 @@
+# Variable = a reusable container for storing a value
+#      a varible behaves as if it were the value it contains
+
+age = 100
+
+# Be mindful of the spaces
+print("I'm "  + str(age) + " this year")
+print("i'm",age, "this year")
+
+# use of f strings to diplay no alongside text. this is widely used.
+print(f"i'm {age} this year")
+
+
+# integers
+age = 21
+players=22
+refeere=4
+
+print(f"The players are {age} years old each")
+print(f"both team must be {players}")
+print(f"for a match we have {refeere} officials")
+
+# floats: Floats in Python are numeric data types that represent numbers with decimal points. 
+
+score=20.5
+price=20.23
+Gpa=4.99
+
+print(f"my total score was {score} during the exam")
+print(f"The price of rice already jumped to {price}")
+print(f"Ade made a CGPA of {Gpa}")
+
+#String: a series of text within quotes,either a single or double quote.
+
+Name='adepase'
+food="fried rice and beans"
+email="adepaseadedolamu123@gmail.com"
+
+print(f"My name is {Name}")
+print(f"my best food is {food}")
+print(f"You can reach my mail at {email}")
+
+# boolean:  
+
+God_dey=True
+satan=False
+th="14th"
+
+print(f"God truly exist... {God_dey}")
+print(f"The devil is a true God,Isnt it? {satan} ")
+print(f"when is Valentine's Day? {th}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
